73dd1cc965756a06a28e716ad8a2d049871991d5
Burak Ercan <n17145570@cs.hacettepe.edu.tr>
2019-04-21 22:08:12 +0300

    Fixes at physNetReal class

--- data/physNetReal.py
+++ data/physNetReal.py
@@ -9,11 +9,11 @@ class PhysNetReal:
         self.seg_dir = osp.join(path, "Segment")
         self.split_file = osp.join(path, "split.txt")
 
-        self.train_data, self.val_data, self.test_data = self.parse_split_file()
+        self.train, self.val, self.test = self.parse_split_file()
 
-        print("Train data count {}".format(len(self.train_data)))
-        print("Val data count {}".format(len(self.val_data)))
-        print("Test data count {}".format(len(self.test_data)))
+        print("Train data count {}".format(len(self.train)))
+        print("Val data count {}".format(len(self.val)))
+        print("Test data count {}".format(len(self.test)))
 
         print("PhysNet Real Dataset Loaded")
 
@@ -23,19 +23,19 @@ class PhysNetReal:
         test = []
         with open(self.split_file) as f:
             for line in f:
-                idx, spl = line.split(" ")
-                if spl == 0:
+                idx, spl = line.split()
+                if int(spl) == 0:
                     continue
-                elif spl > 3:
+                elif int(spl) > 3:
                     print("Wrong split at index {}".format(idx))
                     continue
                 img0_name = "img_concat_" + idx + "_first.png"
-                img0_path = osp(self.img0_dir, img0_name)
+                img0_path = osp.join(self.img0_dir, img0_name)
                 if not osp.isfile(img0_path):
                     print("First image not available at index {}".format(idx))
                     continue
                 img1_name = "img_concat_" + idx + "_last.png"
-                img1_path = osp(self.img1_dir, img1_name)
+                img1_path = osp.join(self.img1_dir, img1_name)
                 if not osp.isfile(img1_path):
                     print("Last image not available at index {}".format(idx))
                     continue
@@ -43,10 +43,10 @@ class PhysNetReal:
                 seg2_name = "imgseg_" + idx + "_B.png"
                 seg3_name = "imgseg_" + idx + "_C.png"
                 seg4_name = "imgseg_" + idx + "_D.png"
-                seg1_path = osp(self.seg_dir, seg1_name)
-                seg2_path = osp(self.seg_dir, seg2_name)
-                seg3_path = osp(self.seg_dir, seg3_name)
-                seg4_path = osp(self.seg_dir, seg4_name)
+                seg1_path = osp.join(self.seg_dir, seg1_name)
+                seg2_path = osp.join(self.seg_dir, seg2_name)
+                seg3_path = osp.join(self.seg_dir, seg3_name)
+                seg4_path = osp.join(self.seg_dir, seg4_name)
                 if not osp.isfile(seg1_path):
                     print("First segmentation not available at index {}".format(idx))
                     continue
@@ -59,11 +59,11 @@ class PhysNetReal:
                 if osp.isfile(seg4_path):
                     seg_paths.append(seg4_path)
                 data = (img0_path, img1_path, seg_paths)
-                if spl == 1:
+                if int(spl) == 1:
                     train.append(data)
-                elif spl == 2:
+                elif int(spl) == 2:
                     val.append(data)
-                elif spl == 3:
+                elif int(spl) == 3:
                     test.append(data)
                 else:
                     print("Unexpected!")
