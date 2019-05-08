1f9043e750592dde455fc1772e4ba120a3334a6f
Burak Ercan <n17145570@cs.hacettepe.edu.tr>
2019-05-09 01:26:14 +0300

    DeConvLayer added to common models

--- model/common.py
+++ model/common.py
@@ -12,6 +12,7 @@ class ConvLayer(torch.nn.Module):
         self.stride = stride
         self.padding = padding
         self.conv = nn.Conv2d(cin, cout, kernel_size, stride, padding)
+        # TODO: weight initialization? nn.init.xavier_uniform(self.conv.weight)
         self.activation = nn.ReLU()
         self.bn = nn.BatchNorm2d(cout)
         self.dropout = nn.Dropout2d(dropout)
@@ -23,6 +24,35 @@ class ConvLayer(torch.nn.Module):
 
     def output_size(self, input_size):
         return int((input_size + 2*self.padding - self.kernel_size) / self.stride) + 1
+
+    def forward(self, input):
+        output = self.net(input)
+        return output
+
+
+class DeConvLayer(torch.nn.Module):
+    """ DeConv Layer Class
+    """
+
+    def __init__(self, cin, cout, kernel_size=3, stride=1, padding=0, out_padding=0, bn=False, dropout=0):
+        super(DeConvLayer, self).__init__()
+        self.kernel_size = kernel_size
+        self.op = out_padding
+        self.stride = stride
+        self.padding = padding
+        self.deconv = nn.ConvTranspose2d(cin, cout, kernel_size, stride, padding, self.op)
+        # TODO: weight initialization? nn.init.xavier_uniform(self.conv.weight)
+        self.activation = nn.ReLU()
+        self.bn = nn.BatchNorm2d(cout)
+        self.dropout = nn.Dropout2d(dropout)
+
+        if bn:
+            self.net = nn.Sequential(self.deconv, self.dropout, self.bn, self.activation)
+        else:
+            self.net = nn.Sequential(self.deconv, self.dropout, self.activation)
+
+    def output_size(self, input_size):
+        return (input_size - 1)*self.stride - 2*self.padding + self.kernel_size + self.op
 
     def forward(self, input):
         output = self.net(input)
