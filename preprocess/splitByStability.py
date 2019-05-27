b116b9cb8d9405d403dada5a0a94bdb6dc2feca8
burakercan <bercan@argosai.com>
2019-05-27 17:33:58 +0300

    New split script

--- preprocess/splitByStability.py
+++ preprocess/splitByStability.py
@@ -18,7 +18,14 @@ stable_scene_count = len(stable)
 num_train = round(stable_scene_count * 0.8)
 num_val = round(stable_scene_count * 0.1)
 num_test = stable_scene_count - num_train - num_val
-            
+
+f= open(split_path, "w+")
+
 for scene in range(scene_count):
     if scene in stable:
-        
+        # TODO: split is 1, 2 or 3
+        pass
+    else:
+        split = 0
+    # f.write(str(scene) + "\t" + str(split))
+# f.close()
