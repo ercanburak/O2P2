c40359221902be351dafc3527d828c52e0b36656
Burak Ercan <n17145570@cs.hacettepe.edu.tr>
2019-05-27 23:57:00 +0300

    splitByStability script done

--- preprocess/splitByStability.py
+++ preprocess/splitByStability.py
@@ -1,9 +1,7 @@
-import os, shutil
-
 metadata_path = "..\\datasets\\PhysNetReal\\all_metadata.txt"
-split_path = "..\\datasets\\PhysNetReal\\split_only_stable.txt"
+split_path = "..\\datasets\\PhysNetReal\\split_only_unstable.txt"
 
-stable = set()
+unstable = set()
 scene_count = 0
 with open(metadata_path, "r") as metadata:
     for scene in metadata:
@@ -11,21 +9,28 @@ with open(metadata_path, "r") as metadata:
         scene_att = scene.split()
         idx = scene_att[0]
         fall = scene_att[18]
-        if not int(fall):
-            stable.add(idx)
+        if int(fall):
+            unstable.add(int(idx))
 
-stable_scene_count = len(stable)
-num_train = round(stable_scene_count * 0.8)
-num_val = round(stable_scene_count * 0.1)
-num_test = stable_scene_count - num_train - num_val
+unstable_scene_count = len(unstable)
+num_train = round(unstable_scene_count * 0.8)
+num_val = round(unstable_scene_count * 0.1)
+num_test = unstable_scene_count - num_train - num_val
 
-f= open(split_path, "w+")
+f = open(split_path, "w+")
+
+count_unstables_added = 0
 
 for scene in range(scene_count):
-    if scene in stable:
-        # TODO: split is 1, 2 or 3
-        pass
+    if scene+1 in unstable:
+        if count_unstables_added < num_train:
+            split = 1
+        elif count_unstables_added < num_train + num_val:
+            split = 2
+        else:
+            split = 3
+        count_unstables_added += 1
     else:
         split = 0
-    # f.write(str(scene) + "\t" + str(split))
-# f.close()
+    f.write(str(scene+1) + "\t" + str(split) + "\n")
+f.close()
